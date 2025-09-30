ebf00cba5ad20c775144064dccbfd59f2276c3de
Francois Lefebvre <23463958+fslef@users.noreply.github.com>
2025-09-30 17:14:17 +0200

    fix: remove routeBasePath from docs configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,6 @@ const config: Config = {
       'classic',
       {
         docs: {
-          routeBasePath: '/',
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
