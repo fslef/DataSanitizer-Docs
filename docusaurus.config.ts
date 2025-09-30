9eb5473af36b7aaacb55b18871751ac7278f0145
Francois Lefebvre <23463958+fslef@users.noreply.github.com>
2025-09-30 17:13:13 +0200

    docs: add new documentation files and update baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://datasanitizer.fslef.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/DataSanitizer-Docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -41,7 +41,6 @@ const config: Config = {
       {
         docs: {
           routeBasePath: '/',
-          homePageId: 'intro',
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
